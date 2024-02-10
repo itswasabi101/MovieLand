aef84d810ae86c554885592b924346d7fe778e2b
Aaron Abraham <ronjohneden@gmail.com>
2024-02-10 12:25:19 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg'
-const API_URL = 'http://www.omdbapi.com?apikey=32b532db'
+const API_URL = 'https://www.omdbapi.com?apikey=32b532db'
 
 // const movie1 = {
 //     "Title": "Fighting, Flying and Driving: The Stunts of Spiderman 3",
