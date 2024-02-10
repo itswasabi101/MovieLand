3d069dce4678bc8895fb7227de8264ec9bb84dfb
Aaron Abraham <ronjohneden@gmail.com>
2024-02-10 12:03:08 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import './App.css';
 import SearchIcon from './search.svg'
 const API_URL = 'http://www.omdbapi.com?apikey=32b532db'
 
-const movie1 = {
-    "Title": "Fighting, Flying and Driving: The Stunts of Spiderman 3",
-    "Year": "2007",
-    "imdbID": "tt1132238",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg"
+// const movie1 = {
+//     "Title": "Fighting, Flying and Driving: The Stunts of Spiderman 3",
+//     "Year": "2007",
+//     "imdbID": "tt1132238",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg"
 
 
-}
+// }
 
 const App = () => {
     const [movies, setMovies] = useState([]);
